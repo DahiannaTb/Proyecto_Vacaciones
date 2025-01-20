ad82eb389cb5ea4c67f46917081259f07d93a163
Zharick <zharicklondos@gmail.com>
2025-01-20 11:27:20 -0500

    realiza la captura del presupuesto inicial imprimiendo en consola.

---
+++ js/codigo.js
@@ -0,0 +1,13 @@
+//capturar el input en donde se pide el presupuesto inicial
+let presupuesto_inicial=document.getElementById('presupuesto_inicial');
+
+//capturar el boton en el que se continúa a la siguiente página
+let boton_continuar=document.getElementById('boton_continuar');
+
+//que el valor del input se guarde solo si el usuario hace click en continuar
+boton_continuar.addEventListener('click', ()=>{
+    let presupuesto=parseInt(presupuesto_inicial.value);
+    console.log(presupuesto);
+})
+
+
